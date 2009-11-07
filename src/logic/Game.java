5a1f4c9d1d6999f1e0af19f83c9e8ea5c0707a6a
metalseer <metalseer@fa1d602a-cba2-11de-a8df-5d31f88aefc0>
2009-11-07 14:16:34 +0000

    Agregue la carpeta resources

--- src/logic/Game.java
+++ src/logic/Game.java
@@ -1,13 +1,11 @@
 package logic;
 
-import gui.GameMenu;
-
 public class Game {
 
 		public static void main(String args[]) {
 
-			GameMenu menu = new GameMenu();
-			menu.setVisible(true);
+			//SampleFrame menu = new SampleFrame();
+			//menu.setVisible(true);
 
 		}
 }
