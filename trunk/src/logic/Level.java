3d798a480e8fd1cb2d75d82c1b32d1adcd331988
alejandromagnorsky <alejandromagnorsky@fa1d602a-cba2-11de-a8df-5d31f88aefc0>
2009-11-08 00:07:26 +0000

    Con Lasers

--- trunk/src/logic/Level.java
+++ trunk/src/logic/Level.java
@@ -25,17 +25,20 @@ public class Level {
 			for (int j = 0; j < tileSet.getCols()
 					&& !(status.getName().equals("GameOver")); j++) {
 				Tile itr = tileSet.at(new Vector2D(i, j));
-				if (itr instanceof Origin) {
+				if (itr instanceof Origin)
 					while (!(status.getName().equals("StopLaser"))
-							&& !(status.getName().equals("GameOver"))
-							&& itr.getPos().getX() < tileSet.getCols()) {
+							&& !(status.getName().equals("GameOver"))) {
 						Tile next = tileSet.at(itr.nextPosition());
 						status = itr.action(next);
 						System.out.println("Status:" + status);
 						System.out.println("Next:" + next.getPos());
 						itr = next;
+						if ( itr.nextPosition().getX() > tileSet.getRows() || itr.nextPosition().getX() < 0 
+							|| itr.nextPosition().getY() > tileSet.getCols() || itr.nextPosition().getY() < 0) //MODIFICAR CON UN METODO QUE SE FIJE SI ESTA DENTRO
+							break;
 					}
-				}
+				if (status.getName().equals("StopLaser"))
+					status = new GameMessage("");
 			}
 	}
 }
