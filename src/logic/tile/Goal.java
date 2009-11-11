ec871f196bad7bf2561e411b01b783c99c9ec62d
alejandromagnorsky <alejandromagnorsky@fa1d602a-cba2-11de-a8df-5d31f88aefc0>
2009-11-11 00:09:34 +0000

    Cambiar el constructor de Goal en tileSet

--- src/logic/tile/Goal.java
+++ src/logic/tile/Goal.java
@@ -3,20 +3,25 @@ package logic.tile;
 import logic.GameMessage;
 import logic.Vector2D;
 import logic.laser.Laser;
+import logic.laser.LaserColor;
 
 public class Goal extends StaticTile {
 
-	public Goal(Vector2D pos) {
+	private LaserColor color;
+
+	public Goal(Vector2D pos, LaserColor color) {
 		super(pos);
+		this.color = color;
+
 	}
 
 	@Override
 	public GameMessage action(Tile t) {
 		if (this.hasLasers()) {
 			t.addLaser(new Laser(getLastLaser()));
-			return new GameMessage("GoalAcchieved");
+			if (color.equals(getLastLaser()))
+				return new GameMessage("GoalAcchieved");
 		}
-
 		return null;
 	}
 
