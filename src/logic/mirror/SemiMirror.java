8dea1f1536b33e432ca2692c6482f9ed5891110b
alejandromagnorsky <alejandromagnorsky@fa1d602a-cba2-11de-a8df-5d31f88aefc0>
2009-11-12 13:12:48 +0000

    Optimizado

---
+++ src/logic/mirror/SemiMirror.java
@@ -0,0 +1,12 @@
+package logic.mirror;
+
+import logic.Vector2D;
+import logic.tile.Tile;
+import messages.GameMessage;
+
+public class SemiMirror extends DoubleMirror{
+
+	public SemiMirror(Vector2D pos, int orientation) {
+		super(pos, orientation);
+	}
+}
