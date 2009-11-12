c91f8b6ae1599226c569ca8e72d02c9560565875
alejandromagnorsky <alejandromagnorsky@fa1d602a-cba2-11de-a8df-5d31f88aefc0>
2009-11-12 14:34:30 +0000

    Con semimirror

--- src/logic/mirror/SemiMirror.java
+++ src/logic/mirror/SemiMirror.java
@@ -1,12 +1,17 @@
 package logic.mirror;
 
 import logic.Vector2D;
-import logic.tile.Tile;
-import messages.GameMessage;
+import logic.laser.Vector2DStack;
 
 public class SemiMirror extends DoubleMirror{
 
 	public SemiMirror(Vector2D pos, int orientation) {
 		super(pos, orientation);
+	}
+	
+	public Vector2DStack nextPosition() {
+		Vector2DStack outStack = super.nextPosition();
+		outStack.add(getPos().add(getLastLaser().getDir()));
+		return outStack;
 	}
 }
