9458266fc39d8a033200a890a5b8515dd34bc4db
alejandromagnorsky <alejandromagnorsky@fa1d602a-cba2-11de-a8df-5d31f88aefc0>
2009-11-14 19:46:24 +0000

    Con PlayerPanel.

--- src/gui/PlayerPanel.java
+++ src/gui/PlayerPanel.java
@@ -1,23 +1,38 @@
 package gui;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
-
-import javax.swing.JFrame;
+import java.awt.Graphics;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import logic.Player;
 
-public class PlayerPanel extends JFrame{
+public class PlayerPanel extends JPanel{
 	
 	private Player player;
+	private JLabel scoreLabel;
 	
 	public PlayerPanel(Player player){
 		this.player = player;
-				
-		Toolkit toolkit = getToolkit();
-		Dimension size = toolkit.getScreenSize();
-		setLocation(size.width / 2 - getWidth() / 2, size.height / 2
-				- getHeight() / 2);
-
+		setLayout(null);
+		setOpaque(false);
+		add(getNameLabel());
+		add(scoreLabel = getScoreLabel());
 	}
+	
+	private JLabel getNameLabel(){
+		JLabel label = new JLabel(player.getName());
+		label.setBounds(5, 20, 100, 30);
+		return label;
+	}
+	
+	private JLabel getScoreLabel(){
+		JLabel label = new JLabel("Puntaje: " + String.valueOf(player.getScore()));
+		label.setBounds(5, 150, 100, 30);
+		return label;
+	}
+	
+	public void paint(Graphics g) {
+		super.paint(g);
+		this.scoreLabel.setText("Puntaje: "+ String.valueOf(player.getScore()));
+	}	
 		
 }
