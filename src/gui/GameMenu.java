7dec07a579a08a7b2b5f848a99b615b75a00f850
metalseer <metalseer@fa1d602a-cba2-11de-a8df-5d31f88aefc0>
2009-11-15 20:23:55 +0000

    El menu carga niveles.

--- src/gui/GameMenu.java
+++ src/gui/GameMenu.java
@@ -2,8 +2,13 @@ package gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 import logic.Level;
 
 public class GameMenu extends JFrame {
@@ -12,57 +17,92 @@ public class GameMenu extends JFrame {
 
 	private ImagePanel background;
 
+	private JFileChooser openDialog;
+
+	private JButton loadButton;
+
+	private String filename;
 
 	public GameMenu() {
 		super("Menu Principal");
-		
+
 		setLayout(null);
-		
+
 		setBounds(100, 100, 900, 725);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JButton start = new JButton("Start game");
-		start.setBounds(getWidth()/2-100, getHeight()/3+50, 200, 50);
-		
+		start.setBounds(getWidth() / 2 - 100, getHeight() / 3 + 50, 200, 50);
+
 		start.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				try {
-					Level level = new Level("levelTest.txt");
-					setVisible(false);
-				} catch (Exception exc) {
-					System.out.println(exc);
-				}
-
+				startLevel();
 			}
 		});
 
-		JButton load = new JButton("Load");
-		load.setBounds(getWidth()/2-100, getHeight()/2, 200, 50);
+		// Default lvl
+		filename = "levelTest.txt";
+
+		JButton loadButton = new JButton("Load");
+		loadButton.setBounds(getWidth() / 2 - 100, getHeight() / 2, 200, 50);
+
+		openDialog = new JFileChooser(new File(""));
+
+		openDialog.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				filename = openDialog.getSelectedFile().getPath();
+				startLevel();
+			}
+
+		});
+
+		loadButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				showOpenDialog();
+			}
+		});
 
 		JButton exit = new JButton("Exit");
-		exit.setBounds(getWidth()/2-100, getHeight()-50-getHeight()/3, 200, 50);
+		exit.setBounds(getWidth() / 2 - 100,
+				getHeight() - 50 - getHeight() / 3, 200, 50);
 		exit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
 			}
 		});
-		ImageButton start2 = null;
-		
+
 		try {
 			background = new ImagePanel("bg.jpg");
 			background.setBounds(0, 0, 900, 725);
-			
+
 		} catch (Exception e) {
 			// ERROR! (create an error frame)
 			System.out.println(e);
 		}
-		
+
 		add(start);
 		add(exit);
-		add(load);
+		add(loadButton);
 		add(background);
-		//add(start2);
-	
+		// add(start2);
+
+	}
+
+	private void startLevel() {
+		try {
+			Level level = new Level(filename);
+			setVisible(false);
+		} catch (Exception exc) {
+			System.out.println(exc);
+			JOptionPane.showMessageDialog(null,
+					"Archivo incorrecto o corrupto.");
+		}
+	}
+
+	private void showOpenDialog() {
+		openDialog.showOpenDialog(loadButton);
 	}
 }
